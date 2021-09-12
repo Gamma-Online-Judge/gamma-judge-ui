d195fc6161e07eb4ff128a7c6f8502724b96e1ae
Gustavo Lima <gustavomarques.gml@gmail.com>
2021-09-12 09:31:15 -0300

    Correction of home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Alert, Button, Card } from "react-bootstrap";
+import { Button, Card } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import Header from "../components/Header";
 
@@ -17,81 +17,58 @@ const HomePage = () => {
         <div>
             <Header />
             <div className="pa2 flex tc flex-column justify-center items-center">
+                <h1> Gamma Online Judge </h1>
+                <p>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+                    The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+                </p>
+            </div>
 
-                {renderAc()}
-                {renderWa()}
-                {renderRte()}
+            <div className="pa2 flex tc flex-wrap justify-center items-center">
+                <Card style={{ width: '18rem' }} className="ma2 tc">
+                    <Card.Img variant="top" src={IMG_LINK_1} />
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button
+                            variant="primary"
+                            onClick={() => goToPage('/contests')}
+                        >
+                            Contests
+                        </Button>
+                    </Card.Body>
+                </Card>
 
+                <Card style={{ width: '18rem' }} className="ma2 tc">
+                    <Card.Img variant="top" src={IMG_LINK_2} />
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button
+                            variant="primary"
+                            onClick={() => goToPage('/problems')}
+                        >
+                            Problems
+                        </Button>
+                    </Card.Body>
+                </Card>
+
+                <Card style={{ width: '18rem' }} className="ma2 tc">
+                    <Card.Img variant="top" src={IMG_LINK_3} />
+                    <Card.Body>
+                        <Card.Text>
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </Card.Text>
+                        <Button variant="primary"> Info </Button>
+                    </Card.Body>
+                </Card>
             </div>
         </div>
     )
 }
 export default HomePage;
-
-const renderAc = () => {
-    return (
-        <Alert variant="success">
-            <h3> AC </h3>
-            <p>
-                Parabéns o seu código passou em todos os casos de teste
-            </p>
-            <hr />
-            <div className="d-flex justify-content-end">
-                <Button variant="outline-success">
-                    Ver o código
-                </Button>
-            </div>
-        </Alert>
-    )
-}
-
-const renderWa = () => {
-    return (
-        <Alert variant="danger">
-            <h3> WA </h3>
-            <p>
-                O seu código falhou em alguns casos de teste
-            </p>
-            <hr />
-            <div className="d-flex justify-content-end">
-                <Button variant="outline-danger">
-                    Ver o código
-                </Button>
-            </div>
-        </Alert>
-    )
-}
-
-const renderTle = () => {
-    return (
-        <Alert variant="warning">
-            <h3> TLE </h3>
-            <p>
-                O seu código não resolveu o problema no limite de tempo
-            </p>
-            <hr />
-            <div className="d-flex justify-content-end">
-                <Button variant="outline-warning">
-                    Ver o código
-                </Button>
-            </div>
-        </Alert>
-    )
-}
-
-const renderRte = () => {
-    return (
-        <Alert variant="secondary">
-            <h3> RTE </h3>
-            <p>
-                Algo anormal aconteceu durante a execução do seu código
-            </p>
-            <hr />
-            <div className="d-flex justify-content-end">
-                <Button variant="outline-secondary">
-                    Ver o código
-                </Button>
-            </div>
-        </Alert>
-    )
-}
